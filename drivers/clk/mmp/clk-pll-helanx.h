a0053eb93a450d12c0c1e6e58705721df2d3c68b
Karel Balej <balejk@matfyz.cz>
2024-07-05 10:16:36 +0200

    Revert "clk: mmp: drop mmp_vco_params"
    This reverts commit 21ce4b053cfeb222e6776ff1e6ad710f19fe74ac as it broke
    stuff and I cannot find the reason why.

--- drivers/clk/mmp/clk-pll-helanx.h
+++ drivers/clk/mmp/clk-pll-helanx.h
@@ -1,11 +1,6 @@
 #ifndef __MACH_MMP_CLK_PLL_HELANX_H
 #define __MACH_MMP_CLK_PLL_HELANX_H
 
-#define MPMU_POSR		0x10
-#define MPMU_POSR_PLL2_LOCK	BIT(29)
-#define MPMU_POSR_PLL3_LOCK	BIT(30)
-#define MPMU_POSR_PLL4_LOCK	BIT(31)
-
 struct kvco_range {
 	int vco_min;
 	int vco_max;
@@ -22,26 +17,21 @@ struct intpi_range {
 	u8 value;
 };
 
-struct mmp_param_vco {
-	unsigned int id;
-	char *name;
-	const char *parent_name;
-	unsigned long clk_flags;
-	u32 vco_flags;
-	unsigned int cr_offset; /* Control Register Offset */
-	unsigned int swcr_offset; /* Software Control Register Offset */
-	spinlock_t *lock;
-	unsigned long default_rate;
+struct mmp_vco_params {
 	unsigned long vco_min;
 	unsigned long vco_max;
+	void __iomem *cr; /* Control Register */
+	void __iomem *swcr; /* Software Control Register */
+	void __iomem *lock_reg;
 	u32 lock_enable_bit;
+	unsigned long default_rate;
 };
 
 struct clk_vco {
 	struct clk_hw hw;
-	void __iomem *mpmu_base;
-	void __iomem *apbs_base;
-	struct mmp_param_vco *params;
+	spinlock_t *lock;
+	u32 flags;
+	struct mmp_vco_params *params;
 };
 
 struct clk_pll {
@@ -61,8 +51,9 @@ struct clk_pll {
 #define HELANX_VCO_28NM			BIT(2)
 #define HELANX_VCO_SKIP_DEF_RATE	BIT(3)
 
-extern struct clk *helanx_register_clk_vco(struct mmp_param_vco *params,
-		void __iomem *mpmu_base, void __iomem *apbs_base);
+extern struct clk *helanx_register_clk_vco(const char *name,
+		const char *parent_name, unsigned long flags, u32 vco_flags,
+		spinlock_t *lock, struct mmp_vco_params *params);
 
 /* PLL flags */
 #define HELANX_PLLOUT			BIT(0)
